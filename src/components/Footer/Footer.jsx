9d112ae63ce5f57cc509e43efac488d16feb6f0d
Facundo Pellicer <facundopellicer4@gmail.com>
2021-11-29 14:33:08 -0300

    readme andfooter button hovers

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,28 +4,38 @@ import { Button } from "@chakra-ui/button";
 
 export default function Footer() {
   return (
-    <Flex justify="center" pt="5%" pb="2%" bg='gray.900'>
+    <Flex justify="center" pt="5%" pb="2%" bg="gray.900">
       <Box>
         <Link
           href="https://www.linkedin.com/in/facundo-pellicer-full-stack-developer/"
           isExternal
+          _hover={{ textDecoration: "none" }}
         >
-          <Button
-            color="cyan.200"
-            variant="ghost"
-          >
+          <Button color="cyan.200" variant="ghost">
             Linkedin
           </Button>
         </Link>
       </Box>
       <Box>
-        <Link href="https://github.com/Facupelli" isExternal>
-          <Button color="cyan.200" variant="ghost">Github</Button>
+        <Link
+          href="https://github.com/Facupelli"
+          isExternal
+          _hover={{ textDecoration: "none" }}
+        >
+          <Button color="cyan.200" variant="ghost">
+            Github
+          </Button>
         </Link>
       </Box>
       <Box>
-        <Link href="https://www.instagram.com/facu_pellicer/" isExternal>
-          <Button color="cyan.200" variant="ghost">Instagram</Button>
+        <Link
+          href="https://www.instagram.com/facu_pellicer/"
+          isExternal
+          _hover={{ textDecoration: "none" }}
+        >
+          <Button color="cyan.200" variant="ghost">
+            Instagram
+          </Button>
         </Link>
       </Box>
     </Flex>
