ee4161855a2ca8f9867aef1aff81e0c7e0f773e5
Facupelli <facundopellicer4@gmail.com>
2021-12-11 21:06:15 -0300

    dogs card style

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -4,6 +4,7 @@ import WebService from "./WebService/WebService";
 import Love from "../Quotes/Love";
 import Matters from "../Quotes/Matters";
 import { ArrowDownIcon } from "@heroicons/react/solid";
+import s from "./Works.module.css";
 
 export default function Works() {
   const [seeMoreDogs, setSeeMoreDogs] = useState(false);
@@ -38,32 +39,45 @@ export default function Works() {
         <h1 className="font-semibold text-3xl text-white">Works</h1>
       </div>
 
-      <div>
+      {/* ----------------------------------- DOGS CARD ------------------------------------------ */}
+      <div className="flex justify-center">
         {!seeMoreDogs && (
-          <div className="flex justify-center ">
-            <div
-              className="group w-1/2 h-96 bg-no-repeat bg-contain rounded-md"
-              style={{ backgroundImage: `url('${dogImage}')` }}
-            >
-              <div className="absolute">
-                <button onClick={handleOnClickDog}>
-                  <div className="flex gap-2 items-center">
-                    <p className="font-bold text-2xl font-body">SEE MORE</p>
-                    <ArrowDownIcon className="h-5 w-5 text-cyan-500" />
-                  </div>
-                </button>
-              </div>
+          // <div>
+          //   <div className={s.card}>
+          //     <img className={s.dogImage} src={dogImage} />
+          //   </div>
+          //   <div className={s.info}>
+          //     <button onClick={handleOnClickDog}>
+          //       <div className="flex justify-center items-center gap-2 font-bold text-3xl text-cyan-400">
+          //         <p>SEE MORE</p>
+          //         <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
+          //       </div>
+          //     </button>
+          //   </div>
+          // </div>
+
+          <div className={s.image}>
+            <img className={s.image__img} src={dogImage} alt="dogs" />
+            <div className={s.image__overlay}>
+              <button onClick={handleOnClickDog}>
+                <div className={s.image__title}>
+                  <div>SEE MORE</div>
+                  <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
+                </div>
+              </button>
             </div>
           </div>
         )}
       </div>
 
+      {/* ----------------------------------- DESPLIEGUE DOGS ------------------------------------------ */}
       <div>
         {seeMoreDogs && (
           <Dogs setSeeMoreDogs={setSeeMoreDogs} seeMoreDogs={seeMoreDogs} />
         )}
       </div>
 
+      {/* ----------------------------------- WEBSERVICE CARD ------------------------------------------ */}
       <div className="pt-20">
         {!seeMoreWeb && (
           <div className="flex justify-center ">
@@ -84,6 +98,7 @@ export default function Works() {
         )}
       </div>
 
+      {/* ----------------------------------- DESPLIEGUE WEBSERVICE ------------------------------------------ */}
       <div className="pt-20">
         {seeMoreWeb && (
           <WebService setSeeMoreWeb={setSeeMoreWeb} seeMoreWeb={seeMoreWeb} />
